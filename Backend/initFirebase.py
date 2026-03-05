4048c0918d83de1ef4771d54ea0cd6057ce068e2
Ben <luxkun31@gmail.com>
2026-03-05 01:21:25 -0500

    Conexion Inventario con Firebase

--- Backend/initFirebase.py
+++ Backend/initFirebase.py
@@ -1,7 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+# Backend/initFirebase.py
+import os
+from firebase_admin import credentials, initialize_app, firestore
 
-cred = credentials.Certificate("Backend/iarecetas-4e7a5-firebase-adminsdk.json")
-firebase_admin.initialize_app(cred)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+key_path = os.path.join(base_dir, "iarecetas-4e7a5-firebase-adminsdk.json")
+
+cred = credentials.Certificate(key_path)
+default_app = initialize_app(cred)
 db = firestore.client()
 
